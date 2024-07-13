d1c32cc64392d2c63ba002cbdfc8451e43f61f86
Nicolas METIVIER <n.metivier@epexspot.com>
2024-07-13 16:59:58 +0200

    Add md preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -166,4 +166,10 @@ return {
       require("rest-nvim").setup()
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
 }
